f0da80250869e95ea3c4dfee4c72f05939f1ee01
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2024-08-26 23:46:44 +0700

    Get the labels form csv and refactor csv.h

--- 2. knn/include/csv.h
+++ 2. knn/include/csv.h
@@ -5,51 +5,116 @@
 #include <stdlib.h>
 #include <string.h>
 
-double** read_csv(char* filepath, int N_SAMPLES, int N_FEATURES){
+double **read_csv(char *filepath, int N_SAMPLES, int N_FEATURES)
+{
+    // Initialize few variables (Buffer and a couples of pointers)
     char buffer[1000];
     char *data;
     char *ptr;
     double value;
 
     // Initialize array of data
-    double** X = (double **)malloc(N_SAMPLES * sizeof(double *));
-    for (int i = 0; i< N_SAMPLES; i++){
+    double **X = (double **)malloc(N_SAMPLES * sizeof(double *));
+    for (int i = 0; i < N_SAMPLES; i++)
+    {
         X[i] = (double *)malloc(N_FEATURES * (sizeof(double)));
     }
 
+    // Open the file
     FILE *fileData = fopen(filepath, "r");
-    
+
     // Check if it's null
-    if(fileData == NULL){
+    if (fileData == NULL)
+    {
         printf("Error could not open file\n");
         exit(-1);
-    } else {
+    }
+    else
+    {
         printf("I was able to open the file\n");
     }
 
     // Get the first line (Column names)
     fgets(buffer, sizeof(buffer), fileData);
 
-    // Get the data
+    // Iterate trough the rows
     int i = 0;
-    while(fgets(buffer, sizeof(buffer), fileData)){
+    while (fgets(buffer, sizeof(buffer), fileData))
+    {
         // Get the id
         strtok(buffer, ",");
-        for (int j = 0; j < N_FEATURES; j++){
+        for (int j = 0; j < N_FEATURES; j++)
+        {
+            // Get the data
             data = strtok(NULL, ",");
-            value = strtod(data, &ptr); 
-            
+            // Convert into double
+            value = strtod(data, &ptr);
+            // Append into arrays
             X[i][j] = value;
-            
+
             // printf("%.2lf\n", value);
-            
         }
         i++;
     }
 
+    // Close the file
     fclose(fileData);
 
     return X;
+}
+
+char **read_csv_label(char *filepath, int N_SAMPLES, int N_FEATURES)
+{
+    // Initialize some variables
+    char buffer[1000];
+    char *data;
+    double value;
+
+    // Initialize array of data
+    char **y = (char **)malloc(N_SAMPLES * sizeof(char *));
+
+    // Open the file
+    FILE *fileData = fopen(filepath, "r");
+
+    // Check if it's null
+    if (fileData == NULL)
+    {
+        printf("Error could not open file\n");
+        exit(-1);
+    }
+    else
+    {
+        printf("I was able to open the file\n");
+    }
+
+    // Get the first line (Column names)
+    fgets(buffer, sizeof(buffer), fileData);
+
+    // Iterate trough the data
+    int i = 0;
+    while (fgets(buffer, sizeof(buffer), fileData))
+    {
+        // Get the id
+        strtok(buffer, ",");
+        for (int j = 0; j < N_FEATURES; j++)
+        {
+            // Get the feature value
+            strtok(NULL, ",");
+        }
+        // Get the label
+        data = strtok(NULL, ",");
+
+        // Allocate memory for the label and copy it
+        y[i] = (char *)malloc(strlen(data) + 1);
+        strcpy(y[i], data);
+
+        i++;
+    }
+
+    // Close the file
+    fclose(fileData);
+
+    return y;
 }
 
 #endif
--- 2. knn/src/main.c
+++ 2. knn/src/main.c
@@ -3,22 +3,28 @@
 #include "knn.h"
 #include "csv.h"
 
-#define N_SAMPLES 100
-#define N_FEATURES 2
+#define N_SAMPLES 150
+#define N_FEATURES 4
 #define LEARNING_RATE 0.1
 #define N_ITERS 5000
 
-int main(){
+int main()
+{
 
+    // READ CSV ------------------------------------------
     double **X = read_csv("iris.csv", 150, 4);
+    char **y = read_csv_label("iris.csv", 150, 4);
 
-    for (int i = 0; i< 150; i++){
-        for (int j = 0; j < 4; j++){
-            printf("%2lf\n",X[i][j]);
-            // printf("Hahaha");
-        }
-    }
+    // for (int i = 0; i < N_SAMPLES; i++)
+    // {
+    //     for (int j = 0; j < N_FEATURES; j++)
+    //     {
+    //         printf("%2lf\n", X[i][j]);
+    //     }
+    //     printf("%s\n", y[i]);
+    // }
 
+    
 
     return 0;
 }
